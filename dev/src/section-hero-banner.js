456bcb1277d202f2dda044e26a6b48774f0ec045
Hryhorii <grigorash514@gmail.com>
2025-02-07 17:22:09 +0200

    Hero Banner

---
+++ dev/src/section-hero-banner.js
@@ -0,0 +1,41 @@
+if (!customElements.get("s-hero-banner")) {
+  customElements.define(
+    "s-hero-banner",
+    class SectioHeroBanner extends HTMLElement {
+      constructor() {
+        super();
+
+      }
+
+      connectedCallback() {
+        this.swiper = new Swiper(this, {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          speed: 400
+        })
+
+        this.toTop = this.classList.contains("js-hero-banner--to-top");
+
+        if(this.toTop) {
+          this.toTopHandler();
+          window.addEventListener("resize", this.toTopHandler)
+        }
+      }
+
+      disconnectedCallback() {
+        window.removeEventListener("resize", this.toTopHandler)
+      }
+
+      toTopHandler = () => {
+        let header = document.querySelector("s-header") || null;
+        let main = document.querySelector("#main-content") || null;
+
+        console.log(header, main)
+
+        if (header && main && window.innerWidth >= 992) {
+          main.style.marginTop = `-${header.offsetHeight}px`
+        }
+      }
+    }
+  );
+}
